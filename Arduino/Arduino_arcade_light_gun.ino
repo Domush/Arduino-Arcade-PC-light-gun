08e3c7bf974de47992cd4638f5a8b6635027259f
Edward Webber <github@webbsense.com>
2021-04-29 01:13:53 -0500

    Added debugging, set proper micro board config

--- Arduino/Arduino_arcade_light_gun.ino
+++ Arduino/Arduino_arcade_light_gun.ino
@@ -5,22 +5,24 @@
  *  @license    GPLv3 - Copyright (c) 2021 Edward Webber
  */
 
-#include <MPU6050_tockn.h>
-#include <Wire.h>
 #include <Mouse.h>
 #include <Keyboard.h>
+#include <Wire.h>
+#include <MPU6050_tockn.h>
 #include <MD_REncoder.h>
 
 // -------- User Settings --------
 // Tuning Options
-const long encoderUpdateRate = 100;   // In milliseconds
-const long mpuUpdateRate     = 5;     // In milliseconds
+const long encoderUpdateRate = 100;    // In milliseconds
+const long mpuUpdateRate     = 1000;   // In milliseconds
 
 // Debug Flags (uncomment to dd)
 #define DEBUG   // Enable to use any prints
-// #define DEBUG_ACCEL
-// #define DEBUG_GYRO
-// #define DEBUG_AIMING
+#ifdef DEBUG
+  #define DEBUG_BUTTONS
+  #define DEBUG_GYRO
+  #define DEBUG_ENCODER
+#endif
 
 // Pin Definitions (do not change)
 const uint8_t buttonTriggerPin = 14;
@@ -75,8 +77,9 @@ void setup() {
   if (digitalRead(buttonTriggerPin) == 0) {
     failsafe();   // Just in-case something goes wrong
   }
-  encoder.begin();
   Mouse.begin();
+  Keyboard.begin();
+  encoder.begin();
   Wire.begin();
   mpu.begin();
   mpu.calcGyroOffsets(true);
@@ -107,39 +110,63 @@ void ProcessButtons() {
       // if encode and trigger buttons both pressed, calibrate gyro
       mpu.calcGyroOffsets(true);
     } else if (!activeTrigger) {
+#ifdef DEBUG_BUTTONS
+      DEBUG_PRINTLN("Pressing left mouse button");
+#endif
       Mouse.press();
       activeTrigger = true;
     }
   } else if (!pressedTrigger && activeTrigger) {
+#ifdef DEBUG_BUTTONS
+    DEBUG_PRINTLN("Releasing left mouse button");
+#endif
     Mouse.release();
     activeTrigger = false;
   }
 
   if (pressedAlt) {
     if (!activeAlt) {
+#ifdef DEBUG_BUTTONS
+      DEBUG_PRINTLN("Pressing right mouse button");
+#endif
       Mouse.press(MOUSE_RIGHT);
       activeAlt = true;
     }
   } else if (!pressedAlt && activeAlt) {
+#ifdef DEBUG_BUTTONS
+    DEBUG_PRINTLN("Releasing right mouse button");
+#endif
     Mouse.release(MOUSE_RIGHT);
     activeAlt = false;
   }
 
   if (pressedReload) {
     if (!activeReload) {
+#ifdef DEBUG_BUTTONS
+      DEBUG_PRINTLN("Pressing R");
+#endif
       Keyboard.press('r');
       activeReload = true;
     }
   } else if (!pressedReload && activeReload) {
+#ifdef DEBUG_BUTTONS
+    DEBUG_PRINTLN("Releasing R");
+#endif
     Keyboard.release('r');
     activeReload = false;
   }
 
   if (pressedEncoder) {
     if (!activeEncoder) {
+#ifdef DEBUG_BUTTONS
+      DEBUG_PRINTLN("Encoder button pressed");
+#endif
       activeEncoder = true;
     }
   } else if (!pressedEncoder && activeEncoder) {
+#ifdef DEBUG_BUTTONS
+    DEBUG_PRINTLN("Encoder button released");
+#endif
     activeEncoder = false;
   }
 }
@@ -147,17 +174,25 @@ void ProcessButtons() {
 void ProcessEncoder() {
   uint8_t encoderPos = encoder.read();
   if (encoderPos) {
+#ifdef DEBUG_ENCODER
     DEBUG_PRINT(encoderPos == DIR_CW ? "\n+1" : "\n-1");
-#if ENABLE_SPEED
+  #if ENABLE_SPEED
     DEBUG_PRINT(" Rate: ");
     DEBUG_PRINTLN(encoder.speed());
-#else
+  #else
     DEBUG_PRINTLN("");
+  #endif
 #endif
     if (encoderPos == DIR_CW) {
+#ifdef DEBUG_ENCODER
+      DEBUG_PRINTLN("Pressing down arrow");
+#endif
       Keyboard.press(KEY_DOWN_ARROW);
       Keyboard.release(KEY_DOWN_ARROW);
     } else if (encoderPos == DIR_CW) {
+#ifdef DEBUG_ENCODER
+      DEBUG_PRINTLN("Pressing up arrow");
+#endif
       Keyboard.press(KEY_UP_ARROW);
       Keyboard.release(KEY_UP_ARROW);
     }
@@ -171,23 +206,32 @@ void ProcessMPU() {
   mpuAngleX = mpu.getAngleX();
   mpuAngleY = mpu.getAngleY();
   mpuAngleZ = mpu.getAngleZ();
-  DEBUG_PRINT("angleX : ");
+#ifdef DEBUG_GYRO
+  DEBUG_PRINT("Gryo X:");
   DEBUG_PRINT(mpuAngleX);
-  DEBUG_PRINT("\tangleY : ");
+  DEBUG_PRINT(" Y:");
   DEBUG_PRINT(mpuAngleY);
-  DEBUG_PRINT("\tangleZ : ");
+  DEBUG_PRINT(" Z:");
   DEBUG_PRINTLN(mpuAngleZ);
-  if (mpuAngleX < 2 || mpuAngleX > 50) {
+#endif
+  if (mpuAngleX < 2 && mpuAngleX > -2 || mpuAngleX > 50 || mpuAngleX < -50) {
     mpuAngleX == 0;
   }
-  if (mpuAngleZ < 2 || mpuAngleZ > 50) {
+  if (mpuAngleZ < 2 && mpuAngleZ > -2 || mpuAngleZ > 50 || mpuAngleZ < -50) {
     mpuAngleZ == 0;
   }
   int mouseMoveX = mpuAngleX * 3;
   int mouseMoveZ = mpuAngleZ * 3;
+#ifdef DEBUG_GYRO
+  DEBUG_PRINT("Moving mouse X:");
+  DEBUG_PRINT(mouseMoveX);
+  DEBUG_PRINT(" and Y:");
+  DEBUG_PRINTLN(mouseMoveZ);
+#else
   Mouse.move(mouseMoveX, mouseMoveZ, 0);
+#endif
 }
 
 void failsafe() {
-  DEBUG_PRINTLN(F("Failsafe triggered. Code will not execute"));
+  DEBUG_PRINTLN("Failsafe triggered. Code will not execute");
 }
